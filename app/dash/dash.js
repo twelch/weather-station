7dd611bf783a9183266417c6f0ef72f70116eb17
Tim Welch <tim.j.welch@gmail.com>
2015-02-04 00:07:57 -0800

    make chart responsive

--- app/dash/dash.js
+++ app/dash/dash.js
@@ -37,7 +37,7 @@ angular.module('weather.dash', ['ngRoute'])
 .directive('linearChart', function($window){
   return{
     restrict:'EA',
-    template:"<svg width='850' height='200'></svg>",
+    template:"<svg width='700' height='300'></svg>",
     link: function(scope, elem, attrs){
       var salesDataToPlot=scope[attrs.chartData];
       var padding = 20;
@@ -78,8 +78,40 @@ angular.module('weather.dash', ['ngRoute'])
             })
             .interpolate("basis");
       }
+
+      /* 
+       * Responsivefy - function to make chart responsive to window resizing
+       * from http://www.brendansudol.com/posts/responsive-d3/
+       */
+      function responsivefy(svg) {
+        // get container + svg aspect ratio
+        var container = d3.select(svg.node().parentNode),
+            width = parseInt(svg.style("width")),
+            height = parseInt(svg.style("height")),
+            aspect = width / height;
+
+        // add viewBox and preserveAspectRatio properties,
+        // and call resize so that svg resizes on inital page load
+        svg.attr("viewBox", "0 0 " + width + " " + height)
+            .attr("perserveAspectRatio", "xMinYMid")
+            .call(resize);
+
+        // to register multiple listeners for same event type, 
+        // you need to add namespace, i.e., 'click.foo'
+        // necessary if you call invoke this function for multiple svgs
+        // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+        d3.select(window).on("resize." + container.attr("id"), resize);
+
+        // get width of container and resize svg to fit it
+        function resize() {
+            var targetWidth = parseInt(container.style("width"));
+            svg.attr("width", targetWidth);
+            svg.attr("height", Math.round(targetWidth / aspect));
+        }
+      }
        
       function drawLineChart() {
+        svg.call(responsivefy);
         setChartParameters();
         svg.append("svg:g")
           .attr("class", "x axis")
