ca285e59ea72979e8d48b8bc40c7d8422dcdbd66
Tim Welch <tim.j.welch@gmail.com>
2015-04-20 22:40:35 -0700

    update build process, fix gulp ctrl chars

--- gulpfile.js
+++ gulpfile.js
@@ -5,39 +5,40 @@ var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var notify       = require('gulp-notify');
 
-var config = {
-     sassPath: './resources/sass',
-    buildPath: './app/assets'
+var config = {  
+  sassPath: './resources/sass',
+  buildPath: './app/assets',
+  bowerDir: './app/assets/depend'
 }
 
-gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest(config.buildPath+'/fonts')); 
+gulp.task('icons', function() {
+  return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+    .pipe(gulp.dest(config.buildPath+'/fonts'));
 });
 
-gulp.task('css', function() { 
-    return gulp.src(config.sassPath + '/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-             includePaths: [
-                 config.sassPath,
-                 config.bowerDir + '/bootstrap-sass/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
-        .on("error", notify.onError(function (error) {
-           return "Error: " + error.message;
-         }))) 
-        .pipe(sourcemaps.write({includeContent: false}))
-        .pipe(sourcemaps.init({loadMaps: true}))
-        //Not working with sourcemaps .pipe(autoprefixer({ browsers: ['last 2 version'] }))
-        .pipe(sourcemaps.write())
-         .pipe(gulp.dest(config.buildPath)); 
+gulp.task('css', function() {
+  return gulp.src(config.sassPath + '/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [               
+        config.sassPath,
+        config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+        config.bowerDir + '/fontawesome/scss',
+      ]
+    })
+    .on("error", notify.onError(function (error) {          
+      return "Error: " + error.message;       
+    })))
+    .pipe(sourcemaps.write({includeContent: false}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    //Not working with sourcemaps .pipe(autoprefixer({ browsers: ['last 2 version'] }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.buildPath));
 });
 
 // Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+gulp.task('watch', function() {
+  gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-  gulp.task('default', ['icons', 'css']);
+gulp.task('default', ['icons', 'css']);
